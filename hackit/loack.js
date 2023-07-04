76edac939a9bbc238448cd187eed7faeb03ded7c
Ratnadeep Bhowmik <112869261+Ratna2@users.noreply.github.com>
2023-07-05 02:10:22 +0530

    Add files via upload

---
+++ hackit/loack.js
@@ -0,0 +1,11 @@
+var password = "quiznation";
+
+        function passcheck() {
+
+
+        if(document.getElementById('pass1').value != password){
+        return false;
+        }
+        if(document.getElementById('pass1').value == password);{
+        }
+        }
